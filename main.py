3ebaea26aac006f96a4cc4b16d244c4e24a3242a
Brayden Bartelds <149531080+braydenmcb@users.noreply.github.com>
2024-09-18 16:48:58 -0600

    Updated main.py
    Added the use of matplot and deepface for expression prediction
    
    -added it to print onto the camera screen to show the emotion and its confidence
    - also prints claiming that there is no face detected if one is not detected in frame.

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import cv2
 import numpy as np
-
+import matplotlib.pyplot as plt
+from deepface import DeepFace
+# https://www.geeksforgeeks.org/facial-expression-detection-using-deepface-module-in-python/     for facial expression detection
+# python3 -m pip install opencv-python-headless numpy deepface matplotlib                        for the venv
 face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 def detect_faces(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray, 1.3, 5)
     if len(faces) == 0:
+        cv2.putText(img, "No Face Found", (50,120), cv2.FONT_HERSHEY_SIMPLEX, 5, (0,0,255), 10)
         return img
     
     for (x,y,w,h) in faces:
         cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 2)
+        result = DeepFace.analyze(img, actions = ['emotion'], enforce_detection=False)
+        result = result[0]
+
+        emotion = result['dominant_emotion']
+        confidence = result['emotion'][emotion]
+        formatted_conf = "{:.{}f}".format(confidence, 4)
+        expression = emotion + " | " + formatted_conf + "% " + "confidence"
+
+        cv2.putText(img, expression, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,0,173), 2)
     return img
 
 cap = cv2.VideoCapture(0)
@@ -23,3 +36,4 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
